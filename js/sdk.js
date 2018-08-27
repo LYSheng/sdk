a7a0180d42687e5378c78eb3dd846de863e39072
Lysheng <15003410020@163.com>
2018-08-27 17:54:28 +0800

    重新规划icon

--- js/sdk.js
+++ js/sdk.js
@@ -59,8 +59,8 @@
                     $("#"+domId+"").qrcode({
                         render : "canvas",    //设置渲染方式，有table和canvas，使用canvas方式渲染性能相对来说比较好
                         text : data.porder,    //扫描二维码后显示的内容,可以直接填一个网址，扫描二维码后自动跳向该链接
-                        width : "100",               //二维码的宽度
-                        height : "100",              //二维码的高度
+                        width : "150",               //二维码的宽度
+                        height : "150",              //二维码的高度
                         background : "#ffffff",       //二维码的后景色
                         foreground : "#000000",        //二维码的前景色
                         src: 'https://lysheng.github.io/sdk/icon.png'             //二维码中间的图片
